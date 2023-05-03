bdf087d11fb0433b3bbfddc7aecd06f8d6687350
Jakub Trnčák <orangebroom@Jakubs-MacBook-Pro.local>
2023-05-03 08:47:46 +0200

    Fix max width of film poster

--- src/components/watchlistCard/WatchlistCard.jsx
+++ src/components/watchlistCard/WatchlistCard.jsx
@@ -16,43 +16,42 @@ function WatchlistCard({records, onRemove }) {
   };
 
   return (
-    <div className="item">
-      <div
-        className="poster-img"
-        style={{
-          background: `url(https://www.themoviedb.org/t/p/w500_and_h282_face${records.backdrop_path}), rgba(0, 0, 0, 0.499)`,
-          backgroundBlendMode: 'multiply',
-          backgroundPosition: "center",
-          backgroundSize: "cover"
-        }}>
-        <h2 className="header">{truncaText}</h2>
-      </div>
-
-      <div className="card"> 
-
-        <div className="stats">
-
-          <div className="popularity">
-            <h4><Icon name='fire-flame-curved' type='fas' color='#ED7A1D' /> {popularity.toFixed(1)}</h4>
-          </div>
-
-          <div className="rating">
-            <h4><Icon name='star' type='fas' color='#ECCE07' /> {(rating.toFixed(1)/2)}/5</h4>
-          </div>
-
-          <div className="save">
-            <button 
-              className="bookmark-remove"
-              onClick={handleRemoveId}>
-              <Icon name="bookmark" type="fas" color="" />
-            </button>
-          </div>
-
+      <div className="item">
+        <div
+          className="poster-img"
+          style={{
+            background: `url(https://www.themoviedb.org/t/p/w500_and_h282_face${records.backdrop_path}), rgba(0, 0, 0, 0.499)`,
+            backgroundBlendMode: 'multiply',
+            backgroundPosition: "center",
+            backgroundSize: "cover"
+          }}>
+          <h2 className="header">{truncaText}</h2>
         </div>
+
+        <div className="card"> 
+
+          <div className="stats">
+
+            <div className="popularity">
+              <h4><Icon name='fire-flame-curved' type='fas' color='#ED7A1D' /> {popularity.toFixed(1)}</h4>
+            </div>
+
+            <div className="rating">
+              <h4><Icon name='star' type='fas' color='#ECCE07' /> {(rating.toFixed(1)/2)}/5</h4>
+            </div>
+
+            <div className="save">
+              <button 
+                className="bookmark-remove"
+                onClick={handleRemoveId}>
+                <Icon name="bookmark" type="fas" color="" />
+              </button>
+            </div>
+
+          </div>
+        </div>
+
       </div>
-
-    </div>
-
   )
 }
 
